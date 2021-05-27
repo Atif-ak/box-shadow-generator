de0020da0e05e8577a72e549014a88a0af461bf9
atif <akatifkhan29@gmail.com>
2021-05-27 15:14:23 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [ver, setVer] = useState(10);
+  const [hor, setHor] = useState(10);
+  const [blur, setBlur] = useState(5);
+  const [col, setCol] = useState("grey");
   return (
+    <>
+    <h1 className="heading">Box Shadow Generator</h1>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="slides">
+        <label>Vertical length : </label>
+        <input
+          type="range"
+          min="-200"
+          max="200"
+          value={ver}
+          onChange={(e) => setVer(e.target.value)}
+        />
+        <label>Horizontal length : </label>
+        <input
+          type="range"
+          min="-200"
+          max="200"
+          value={hor}
+          onChange={(e) => setHor(e.target.value)}
+        />
+        <label>Blur : </label>
+        <input
+          type="range"
+          min="0"
+          max="200"
+          value={blur}
+          onChange={(e) => setBlur(e.target.value)}
+        />
+        <label>Color : </label>
+        <input
+          type="color"
+          value={col}
+          onChange={(e) => setCol(e.target.value)}
+        />
+        <div className="output">
+        <p className="para"> For Developer: {`box-shadow: ${hor}px ${ver}px ${blur}px ${col} ;`}</p>
+        <p className="para"> Keep developing!</p>
+        </div>
+
+      </div>
+      <div className="box" style={{boxShadow:`${hor}px ${ver}px ${blur}px ${col}`}}>
+
+      </div>
     </div>
+    </>
   );
 }
 
